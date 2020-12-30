5296c4774769427dd05da065e98ff7b79311e5ec
azulzaietz <azulzaietz@gmail.com>
2020-12-29 23:12:24 -0300

    documentacion

--- server_src/communication/thread_acceptor.h
+++ server_src/communication/thread_acceptor.h
@@ -13,10 +13,33 @@
 
 class ThreadAcceptor: public Thread {
     //Socket socket;
+
+    /** 
+     * @brief Numero siempre creciente
+     * para asignar un ID numerico
+     * a cada cliente nuevo
+     */
     int clients_counter;
+
+    //TODO crear clase DataBase
+
+    /**
+     * @brief Base de datos que asocia el numero 
+     * de cliente con su hilo correspondiente
+     */
     std::unordered_map<int, ThreadClient*> clientsThreads;
+
+    /**
+     * @brief Base de datos que asocia
+     * el numero de cliente con su hilo partida
+     * correspondiente
+     */
     std::unordered_map<int, ThreadGame&> clientsGames;
 
+    /**
+     * @brief Base de datos que almacena
+     * las partidas en juego
+     */
     int games_counter;
     std::unordered_map<int, ThreadGame*> games;
 
@@ -29,6 +52,7 @@ class ThreadAcceptor: public Thread {
     ThreadAcceptor(/*const Socket& socket, */);
 
     virtual void run() override;
+
     /**
      * @brief Inicia un nuevo hilo cliente y guarda el mismo en
      * el mapa de hilos cliente, usando como clave
@@ -38,6 +62,10 @@ class ThreadAcceptor: public Thread {
     */
     void newClient();
 
+    /**
+     * @brief Parsea el mensaje recibido y efectúa 
+     * la acción correspondiente.
+     */
     void newMessage(std::string message, int clientID);
 
     //void stop();
