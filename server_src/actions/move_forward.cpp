4ce42d74d4b9a58f17d516cb89e4c97d636e0577
pablo.berrotaran <pberrotaran@keepcon.com>
2021-02-13 20:43:32 -0300

    Fix mirroring in movement

--- server_src/actions/move_forward.cpp
+++ server_src/actions/move_forward.cpp
@@ -7,9 +7,12 @@ MoveForward::MoveForward() {}
 
 void MoveForward::tryAction(GameStatus& gs, int clientID){
     Vector pos = gs.getPosition(clientID);
-		Vector direction = gs.getDirection(clientID);
+    Vector direction = gs.getDirection(clientID);
     direction.turnUnitary();
-    Vector next_position = pos + (direction * this->step);
+
+    Vector next_position { pos.x + (direction.x * this->step),
+                           pos.y - (direction.y * this->step) };
+
     is_colision(clientID, next_position, gs);
 }
 
