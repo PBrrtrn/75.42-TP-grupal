a1b5362e8c8f68e76e44e6ac8c945799a66e08e4
pablo.berrotaran <pberrotaran@keepcon.com>
2021-02-14 16:43:06 -0300

    Refactor GameInputHandler

--- client_src/event_handling/GameInputHandler.cpp
+++ client_src/event_handling/GameInputHandler.cpp
@@ -1,5 +1,7 @@
 #include "GameInputHandler.h"
 
+const SDL_Keycode mapped_keys[] = { SDLK_w, SDLK_a, SDLK_s, SDLK_d };
+
 GameInputHandler::GameInputHandler(ServerConnection& server_connection,
 																	 BlockingQueue<MessageType>& message_queue)
 // Todo: Sacar message_queue, que ServerConnection haga los bloqueos
@@ -8,30 +10,44 @@ GameInputHandler::GameInputHandler(ServerConnection& server_connection,
 GameInputHandler::~GameInputHandler() { }
 
 void GameInputHandler::handle(SDL_Event input) {
-	switch (input.type) {
-    case (SDL_KEYDOWN) : 
-      this->keyboard_state.toggleKeyDown(input.key.keysym.sym);
-      break;
-    case (SDL_KEYUP) :
-      this->keyboard_state.toggleKeyUp(input.key.keysym.sym);
-      break;
+	if (input.key.repeat == 0) {
+		SDL_Keycode keycode = input.key.keysym.sym;
+		if (input.type == SDL_KEYDOWN) {
+			if (!this->keyboard_state.isDown(keycode)) {
+				switch (keycode) {
+					case (SDLK_w) : 
+						this->message_queue.push(TYPE_MOVE_FORWARD_START);
+						break;
+					case (SDLK_a) : 
+						this->message_queue.push(TYPE_MOVE_LEFT_START);
+						break;
+					case (SDLK_s) : 
+						this->message_queue.push(TYPE_MOVE_BACKWARD_START);
+						break;
+					case (SDLK_d) : 
+						this->message_queue.push(TYPE_MOVE_RIGHT_START);
+						break;
+				}
+			}
+		  this->keyboard_state.toggleKeyDown(keycode);
+		} else {
+			if (this->keyboard_state.isDown(keycode)) {
+				switch (keycode) {
+					case (SDLK_w) : 
+						this->message_queue.push(TYPE_MOVE_FORWARD_STOP);
+						break;
+					case (SDLK_a) : 
+						this->message_queue.push(TYPE_MOVE_LEFT_STOP);
+						break;
+					case (SDLK_s) : 
+						this->message_queue.push(TYPE_MOVE_BACKWARD_STOP);
+						break;
+					case (SDLK_d) : 
+						this->message_queue.push(TYPE_MOVE_RIGHT_STOP);
+						break;
+				}
+			}
+		  this->keyboard_state.toggleKeyUp(keycode);
+		}
   }
-  
-	std::vector<MessageType> events;
-	if (this->keyboard_state.isDown(SDLK_w)){
-		MessageType type = TYPE_MOVE_FORWARD;
-		this->message_queue.push(type);
-	}
-	if (this->keyboard_state.isDown(SDLK_a)){
-		MessageType type = TYPE_MOVE_LEFT;
-		this->message_queue.push(type);
-	}
-	if (this->keyboard_state.isDown(SDLK_s)){
-		MessageType type = TYPE_MOVE_BACKWARD;
-		this->message_queue.push(type);
-	}
-	if (this->keyboard_state.isDown(SDLK_d)){
-		MessageType type = TYPE_MOVE_RIGHT;
-		this->message_queue.push(type);
-	}	
 }
